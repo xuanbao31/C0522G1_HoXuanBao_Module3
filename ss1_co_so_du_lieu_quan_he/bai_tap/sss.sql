1c5b0b35c853bda1e746dc78cf0aaaacf9899baa
xuanbao31 <baohxgbd210054@fpt.edu.vn>
2022-08-08 14:03:38 +0700

    update

---
+++ ss1_co_so_du_lieu_quan_he/bai_tap/sss.sql
@@ -0,0 +1,14 @@
+create DATABASE school_management;
+USE school_management;
+CREATE TABLE class(
+id int,
+`name`varchar(50)
+);
+
+CREATE TABLE teacher(
+id int,
+`name` varchar(50),
+age int ,
+`country` varchar(50)
+);
+
